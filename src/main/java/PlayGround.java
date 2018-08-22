225fb4c8fc59d859b4ddc7d3c4a5c004c04f6813
saques <msaques@itba.edu.ar>
2018-08-21 23:05:33 -0300

    relative paths

--- src/main/java/PlayGround.java
+++ src/main/java/PlayGround.java
@@ -23,32 +23,30 @@ public final class PlayGround {
      */
 
     public static void contrastEnhancement() throws IOException {
-        Image lena = new Raw(256, 256, Encoding.GS, "D:\\git\\antaim\\images\\LENA.RAW");
-        ImageIO.write(lena.automaticContrastEnhancement(0).toBufferedImage(), "bmp", new File("D:\\git\\antaim\\images\\LENA_ENHANCED.BMP"));
+        Image lena = new Raw(256, 256, Encoding.GS, "images\\LENA.RAW");
+        ImageIO.write(lena.automaticContrastEnhancement(0).toBufferedImage(), "bmp", new File("images\\LENA_ENHANCED.BMP"));
     }
 
     public static void negative() throws IOException {
-        //Image barco = new Raw(290, 207, Encoding.GS, "D:\\git\\antaim\\images\\BARCO.RAW");
-        //ImageIO.write(barco.negative(0).toBufferedImage(), "bmp", new File("D:\\git\\antaim\\images\\BARCO_NEGATIVE.BMP"));
-        Image barco = new Raw(290, 207, Encoding.GS, "C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\BARCO.RAW");
-        ImageIO.write(barco.negative(0).toBufferedImage(), "bmp", new File("C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\BARCO_NEGATIVE.BMP"));
+        Image barco = new Raw(290, 207, Encoding.GS, "images\\BARCO.RAW");
+        ImageIO.write(barco.negative(0).toBufferedImage(), "bmp", new File("images\\BARCO_NEGATIVE.BMP"));
     }
 
     public static void thresholding(double u) throws IOException {
-        Image barco = new Raw(290, 207, Encoding.GS, "C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\BARCO.RAW");
-        ImageIO.write(barco.thresholding(0,u).toBufferedImage(), "bmp", new File("C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\BARCO_THRESHOLDED.BMP"));
+        Image barco = new Raw(290, 207, Encoding.GS, "images\\BARCO.RAW");
+        ImageIO.write(barco.thresholding(0,u).toBufferedImage(), "bmp", new File("images\\BARCO_THRESHOLDED.BMP"));
     }
 
     public static void productWithScalar(double scalar) throws IOException {
-        Image barco = new Raw(290, 207, Encoding.GS, "C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\BARCO.RAW");
-        ImageIO.write(barco.productWithScalar(0,scalar).toBufferedImage(), "bmp", new File("C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\BARCO_PRODUCT_SCALAR.BMP"));
+        Image barco = new Raw(290, 207, Encoding.GS, "images\\BARCO.RAW");
+        ImageIO.write(barco.productWithScalar(0,scalar).toBufferedImage(), "bmp", new File("images\\BARCO_PRODUCT_SCALAR.BMP"));
     }
 
     public static void equalization() throws IOException {
-        Image lena = new Raw(290, 207, Encoding.GS, "C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\LENA.RAW");
-        ImageIO.write(lena.equalization(0).toBufferedImage(), "bmp", new File("C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\LENA_EQUALIZATED2.BMP"));
+        Image lena = new Raw(290, 207, Encoding.GS, "images\\LENA.RAW");
+        ImageIO.write(lena.equalization(0).toBufferedImage(), "bmp", new File("images\\LENA_EQUALIZATED2.BMP"));
         double[] histogram =  lena.equalization(0).histogram(0);
-        PrintWriter writer = new PrintWriter(new FileWriter("C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\histogram_lena_2.txt"));
+        PrintWriter writer = new PrintWriter(new FileWriter("images\\histogram_lena_2.txt"));
         for(int i = 0; i < histogram.length; i++)
             writer.println(histogram[i]);
         writer.flush();
@@ -56,9 +54,9 @@ public final class PlayGround {
     }
 
     public static void equalizationTest() throws IOException {
-        Image lena = new Raw(256, 256, Encoding.GS, "C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\LENA.RAW");
+        Image lena = new Raw(256, 256, Encoding.GS, "images\\LENA.RAW");
 
-        PrintWriter writer = new PrintWriter(new FileWriter("C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\c2Lena.txt"));
+        PrintWriter writer = new PrintWriter(new FileWriter("images\\c2Lena.txt"));
 
         double[] histogram =  lena.equalizatedHisto(0);
         for(int i = 0; i < histogram.length; i++)
@@ -67,9 +65,9 @@ public final class PlayGround {
 
         Image aux =  lena.equalization(0);
          histogram = aux.histogram(0);
-        ImageIO.write(aux.toBufferedImage(), "bmp", new File("C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\LENA_EQUALIZATED2.BMP"));
+        ImageIO.write(aux.toBufferedImage(), "bmp", new File("images\\LENA_EQUALIZATED2.BMP"));
 
-        writer = new PrintWriter(new FileWriter("C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\c1Lena.txt"));
+        writer = new PrintWriter(new FileWriter("images\\c1Lena.txt"));
         for(int i = 0; i < histogram.length; i++)
             writer.println(histogram[i]);
 
@@ -81,12 +79,9 @@ public final class PlayGround {
     }
 
     public static void histogram(boolean negative) throws IOException {
-       // Image barco = new Raw(290, 207, Encoding.GS, "D:\\git\\antaim\\images\\BARCO.RAW");
-        //double[] histogram = negative ? barco.negative(0).histogram(0) : barco.histogram(0);
-        //PrintWriter writer = new PrintWriter(new FileWriter("D:\\git\\antaim\\images\\histogram_barco" + (negative ? "_negative.txt" : ".txt")));
-        Image barco = new Raw(290, 207, Encoding.GS, "C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\BARCO.RAW");
+        Image barco = new Raw(290, 207, Encoding.GS, "images\\BARCO.RAW");
         double[] histogram = negative ? barco.negative(0).histogram(0) : barco.histogram(0);
-        PrintWriter writer = new PrintWriter(new FileWriter("C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\histogram_barco" + (negative ? "_negative.txt" : ".txt")));
+        PrintWriter writer = new PrintWriter(new FileWriter("images\\histogram_barco" + (negative ? "_negative.txt" : ".txt")));
         for(int i = 0; i < histogram.length; i++)
             writer.println(histogram[i]);
         writer.flush();
@@ -94,9 +89,9 @@ public final class PlayGround {
     }
 
     public static void histogramLena() throws IOException {
-        Image lena = new Raw(290, 207, Encoding.GS, "C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\LENA.RAW");
+        Image lena = new Raw(290, 207, Encoding.GS, "images\\LENA.RAW");
         double[] histogram =  lena.histogram(0);
-        PrintWriter writer = new PrintWriter(new FileWriter("C:\\Users\\Nicolas\\Documents\\GitHub\\antaim\\images\\histogram_lena.txt"));
+        PrintWriter writer = new PrintWriter(new FileWriter("images\\histogram_lena.txt"));
         for(int i = 0; i < histogram.length; i++)
             writer.println(histogram[i]);
         writer.flush();
@@ -108,38 +103,38 @@ public final class PlayGround {
     public static void drawMonochromeCircle() throws IOException {
         Image image = new Image(200,200, Encoding.GS, true);
         ImageDrawingUtils.drawCircle(image, 99,99,50, (x, y) -> new double[]{1,1,1});
-        ImageIO.write(image.toBufferedImage(), "bmp", new File("D:\\git\\antaim\\images\\CIRCLE.BMP"));
+        ImageIO.write(image.toBufferedImage(), "bmp", new File("images\\CIRCLE.BMP"));
     }
 
     public static void drawMonochromeSquare() throws IOException{
         Image image = new Image(200,200, Encoding.GS, true);
         ImageDrawingUtils.drawRectangle(image, 50,50, 150, 150, (x, y) -> new double[]{1,1,1});
-        ImageIO.write(image.toBufferedImage(), "bmp", new File("D:\\git\\antaim\\images\\SQUARE.BMP"));
+        ImageIO.write(image.toBufferedImage(), "bmp", new File("images\\SQUARE.BMP"));
     }
 
     public static void drawGrayScale() throws IOException{
         ImageIO.write(ImageDrawingUtils.scale(Encoding.GS, ImageDrawingUtils.grayScale).toBufferedImage(),
-                "bmp", new File("D:\\git\\antaim\\images\\GRAYSCALE.BMP"));
+                "bmp", new File("images\\GRAYSCALE.BMP"));
     }
 
     public static void selectTEST() throws IOException{
-        Image image = new Pgm("/home/nmarcantonio/antaim/images/TEST.PGM");
+        Image image = new Pgm("images\\TEST.PGM");
         ImageIO.write(image.copy(9,69,53,96).toBufferedImage(),
-                "bmp", new File("/home/nmarcantonio/antaim/images/SFASFASFAS.PGM"));
+                "bmp", new File("images\\FASFASFAS.PGM"));
     }
 
     public static void drawFlashCircle() throws IOException{
         Image image = ImageDrawingUtils.drawCircle(
                 ImageDrawingUtils.scale(Encoding.HSV,ImageDrawingUtils.fixedRGB(2, 1)),127,127, 50, ImageDrawingUtils.fixedRGB(1, 1));
-        ImageIO.write(image.toBufferedImage(), "bmp", new File("D:\\git\\antaim\\images\\FLASH.BMP"));
+        ImageIO.write(image.toBufferedImage(), "bmp", new File("images\\FLASH.BMP"));
     }
 
     public static void drawCircleWithImage() throws IOException{
-        Image barco = new Raw(290, 207, Encoding.GS, "D:\\git\\antaim\\images\\BARCO.RAW");
+        Image barco = new Raw(290, 207, Encoding.GS, "images\\BARCO.RAW");
         ImageDrawingUtils.GradientColour colour = barco::getComponents;
         Image image = ImageDrawingUtils.drawCircle(
                 ImageDrawingUtils.scale(Encoding.HSV,ImageDrawingUtils.fixedRGB(2, 1)),127,127, 50, colour);
-        ImageIO.write(image.toBufferedImage(), "bmp", new File("D:\\git\\antaim\\images\\USAIHDKSA.BMP"));
+        ImageIO.write(image.toBufferedImage(), "bmp", new File("images\\USAIHDKSA.BMP"));
     }
 
 
